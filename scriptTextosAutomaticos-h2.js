9a07daf458b0dc8f3f8c27dc051fbbdf8e36f01f
Aloir Lucas <99696063+aloirlucas@users.noreply.github.com>
2022-06-15 20:49:05 -0300

    0101

--- scriptTextosAutomaticos-h2.js
+++ scriptTextosAutomaticos-h2.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
         speed: 200,        
         loop: true
     })
-    .type('Web', {delay: 500}).delete(3)
-    .type('ReactNative', {delay: 900}).delete(11)
+    .type('Web', {delay: 700}).delete(3)
+    .type('React Native', {delay: 900}).delete(11)
     .type('Front-end', {delay: 900}).delete(9)
     .go()
 })
