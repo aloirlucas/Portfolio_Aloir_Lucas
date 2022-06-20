cb00cfdcf5cbc931652244ee043098e01c1b7f2f
Aloir Lucas <99696063+aloirlucas@users.noreply.github.com>
2022-06-20 16:17:11 -0300

    Update scriptMenu.js

--- scriptMenu.js
+++ scriptMenu.js
@@ -1,10 +1,19 @@
+let barraMenuAberto = document.getElementById('barra-menu') /*essa variavel recebe o id do barra menu*/
+
+
 function abrirMenu(){
 
-    let barraMenuAberto = document.getElementById('barra-menu') /*essa variavel recebe o id do barra menu*/
-
+    
     if(barraMenuAberto.style.width == '0px'){  /*Quandio clicar, se o menu estiver 0px, ele vai pra 180px, se não ele vai pra zero*/
-        barraMenuAberto.style.width = '180px'
+        barraMenuAberto.style.width = '135px'
     } else{
         barraMenuAberto.style.width = '0px'
     }
 };
+
+function esconderMenu(){
+    let esconderMenu = document.getElementById('linksMenu')
+    if(barraMenuAberto.style.width == '135px'){
+        barraMenuAberto.style.width = '0px';
+    }
+}
